0d1d383424b32746b7e262fff5bea05ebca1aa4a
javad <javadsaeid8@gmail.com>
2025-12-18 15:23:15 +0330

    init project

---
+++ src/main/java/com/miniFin/minFin/MinFinApplication.java
@@ -0,0 +1,13 @@
+package com.miniFin.minFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinFinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniFin/minFin/MinFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniFin.minFin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
