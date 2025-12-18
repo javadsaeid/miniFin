e26ff18af9c143befa5298f384defc7c7cb300b5
javad <javadsaeid8@gmail.com>
2025-12-18 21:01:32 +0330

    add enums

---
+++ src/main/java/com/miniFin/minFin/enums/NotificationType.java
@@ -0,0 +1,5 @@
+package com.miniFin.minFin.enums;
+
+public enum NotificationType {
+    EMAIL, SMS, PUSH
+}
---
+++ src/main/java/com/miniFin/minFin/enums/TransactionType.java
@@ -0,0 +1,7 @@
+package com.miniFin.minFin.enums;
+
+public enum TransactionType {
+    DEPOSIT,
+    WITHDRAW,
+    TRANSFER,
+}
