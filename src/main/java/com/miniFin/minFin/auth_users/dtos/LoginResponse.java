26ffd1f114c3a01e97a169c9db3a95b3cb3a1cee
javad <javadsaeid8@gmail.com>
2025-12-19 16:03:30 +0330

    add login / register request dto

---
+++ src/main/java/com/miniFin/minFin/auth_users/dtos/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.miniFin.minFin.auth_users.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class LoginResponse {
+
+    private String token;
+    private List<String> roles;
+}
---
+++ src/main/java/com/miniFin/minFin/auth_users/dtos/RegistrationRequest.java
@@ -0,0 +1,23 @@
+package com.miniFin.minFin.auth_users.dtos;
+
+import com.miniFin.minFin.role.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class RegistrationRequest {
+    @NotBlank(message = "FirstName is required")
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    @NotBlank(message = "Email is required")
+    @Email
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+    private List<Role> roles;
+}
