a4c15146e9111eb4cba459ae8e7817db91301489
javad <javadsaeid8@gmail.com>
2026-01-04 09:10:37 +0330

    implement AccountController

--- src/main/java/com/miniFin/minFin/account/controller/AccountController.java
+++ src/main/java/com/miniFin/minFin/account/controller/AccountController.java
@@ -1,9 +1,28 @@
 package com.miniFin.minFin.account.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.miniFin.minFin.account.dtos.AccountDTO;
+import com.miniFin.minFin.account.service.AccountService;
+import com.miniFin.minFin.res.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
 public class AccountController {
+    private final AccountService accountService;
+
+    @GetMapping("/me")
+    public ResponseEntity<Response<List<AccountDTO>>> myAccounts() {
+        return ResponseEntity.ok(accountService.getMyAccounts());
+    }
+
+    @DeleteMapping("/close/{accountNumber}")
+    public ResponseEntity<Response<?>> closeAccount(@PathVariable String accountNumber) {
+        return ResponseEntity.ok(accountService.closeAccount(accountNumber));
+    }
+
 }
