15ab2da400f42462d2e58647b109aa95e9951f08
javad <javadsaeid8@gmail.com>
2026-06-13 07:49:46 +0330

    implement custom router for roles

---
+++ src/front-end/src/services/Guard.js
@@ -0,0 +1,14 @@
+import {Navigate, useLocation} from "react-router-dom";
+import {apiService} from "./api.js";
+
+export const CustomerRoute = ({element: Component}) => {
+    const location = useLocation();
+
+    return apiService.isCustomer() ? (Component) : <Navigate to="/login" replace state={{from: location}} />;
+}
+
+export const AuditorRoute = ({element: Component}) => {
+    const location = useLocation();
+
+    return apiService.isAdmin() || apiService.isAuditor() ? (Component) : <Navigate to="/login" replace state={{from: location}} />;
+}
